13af202ce53359d03e1c41961a23001a163e1e3a
KHI(home) <alfkcjstk853@naver.com>
2023-12-09 00:48:46 +0900

    동시에 슬라이드 타일을 밟아도 움직이게 변경

--- SlideTile.cpp
+++ SlideTile.cpp
@@ -1,6 +1,6 @@
 #include "SlideTile.h"
 void SlideTile::handle_collision(Cube& cube) {
-	while (!cube.try_slide(dir));
+	cube.try_slide(dir);
 }
 
 SlideTile::SlideTile(const string& dir, float x, float z) :Tile(x, z), dir{ dir }{img_name = img_map[dir]; }
